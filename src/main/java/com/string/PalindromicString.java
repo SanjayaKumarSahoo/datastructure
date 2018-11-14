4265f74ce742b9216a39f745065205a25959f2d0
SanjayaKumarSahoo <sahoo.skumar@gmail.com>
2018-11-14 08:14:24 +0530

    [Sanjaya] : some more problem

--- src/main/java/com/string/PalindromicString.java
+++ src/main/java/com/string/PalindromicString.java
@@ -3,7 +3,9 @@ package com.string;
 public class PalindromicString {
 
     public static void main(String[] args) {
-        System.out.println(isPalindromic("ABCDCBA"));
+        System.out.println(isPalindromic("A man, a plan, a canal, Panama."));
+        System.out.println(isPalindromic("Able was I, ere I saw Elba!"));
+        System.out.println(isPalindromic("Ray a Ray"));
     }
 
     private static boolean isPalindromic(String first) {
@@ -12,9 +14,21 @@ public class PalindromicString {
         int end = charArray.length - 1;
 
         while (start < end) {
-            if (charArray[start] != charArray[end]) {
+
+            if (!Character.isLetter(charArray[start])) {
+                start++;
+                continue;
+            }
+
+            if (!Character.isLetter(charArray[end])) {
+                end--;
+                continue;
+            }
+
+            if (Character.toLowerCase(charArray[start]) != Character.toLowerCase(charArray[end])) {
                 return false;
             }
+
             start++;
             end--;
         }
