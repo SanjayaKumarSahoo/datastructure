9755de7e9baa231d764f959d4e6e0ed6606ac312
SanjayaKumarSahoo <sahoo.skumar@gmail.com>
2018-11-21 17:36:31 +0530

    [Sanjaya] : some more problem

--- src/main/java/com/array/KthLargest.java
+++ src/main/java/com/array/KthLargest.java
@@ -3,39 +3,36 @@ package com.array;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
-public class KthSmallest {
+public class KthLargest {
 
-    public static int kthSmallest(int[] data, int k) {
-        return -1;
-    }
 
     public static void main(String[] args) {
         int[] data = {1, 2, -1, 4, 3};
-        System.out.println(findKthSmallest(data, 3));
+        System.out.println(findKthLargest(data, 2));
     }
 
-    private static int findKthSmallest(int[] data, int k) {
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>(new Comparator<Integer>() {
+    private static int findKthLargest(int[] data, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
                 if (o1 == o2) {
                     return 0;
                 } else if (o1 > o2) {
-                    return 1;
-                } else {
                     return -1;
+                } else {
+                    return 1;
                 }
             }
         });
 
         for (int i = 0; i < data.length; i++) {
-            minHeap.add(data[i]);
+            maxHeap.add(data[i]);
         }
 
         for (int i = 0; i < k - 1; i++) {
-            minHeap.poll();
+            maxHeap.poll();
         }
 
-        return minHeap.peek();
+        return maxHeap.peek();
     }
 }
