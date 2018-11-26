89ea730844d9908c8222f7c70975e8cc25e80830
SanjayaKumarSahoo <sahoo.skumar@gmail.com>
2018-11-26 08:52:51 +0530

    [Sanjaya] : some more problem

--- src/main/java/com/primitive/PalindromeDigit.java
+++ src/main/java/com/primitive/PalindromeDigit.java
@@ -3,10 +3,21 @@ package com.primitive;
 public class PalindromeDigit {
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(1221));
     }
 
+    // if the reverse of digit is same as the digit
     private static boolean isPalindrome(int digit) {
-        return true;
+        return digit == reverse(digit);
+    }
+
+    private static int reverse(int digit) {
+        int result = 0;
+        int remaining = Math.abs(digit);
+        while (remaining != 0) {
+            result = result * 10 + remaining % 10;
+            remaining = remaining / 10;
+        }
+        return result;
     }
 }
