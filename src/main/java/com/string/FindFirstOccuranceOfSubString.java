89050662d70e126f3a40164cee81ffd74dfdac19
sahoo.skumar@gmail.com <Sushama#2621>
2021-01-10 21:21:10 +0530

    [String] : Removing consecutive

--- src/main/java/com/string/FindFirstOccuranceOfSubString.java
+++ src/main/java/com/string/FindFirstOccuranceOfSubString.java
@@ -6,7 +6,48 @@ public class FindFirstOccuranceOfSubString {
         String sentence = "vcxvxc agood";
         String search = "good";
         System.out.println(findFirstOccurrence(sentence, search));
+        System.out.println(findFirstOccurrenceOtherMethod(sentence, search));
     }
+
+
+    private static int findFirstOccurrenceOtherMethod(String sentence, String search) {
+        if (sentence == null || sentence.length() == 0) {
+            throw new RuntimeException("Invalid input");
+        }
+        if (search == null || search.length() == 0) {
+            return -1;
+        }
+        char[] sentenceArray = sentence.toCharArray();
+        char[] searchArray = search.toCharArray();
+
+        for (int i = 0; i < sentenceArray.length; i++) {
+            for (int j = 0; j < searchArray.length; j++) {
+                if (sentenceArray[i] != searchArray[j]) {
+                    continue;
+                } else {
+                    boolean isMatch = matchWords(i, sentenceArray, search);
+                    if (isMatch) {
+                        return i;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    private static boolean matchWords(int i, char[] sentenceArray, String search) {
+        int j = 0;
+        char[] searchArray = search.toCharArray();
+        while (i < sentenceArray.length && j < searchArray.length) {
+            if (sentenceArray[i] != searchArray[j]) {
+                return false;
+            }
+            i++;
+            j++;
+        }
+        return true;
+    }
+
 
     private static int findFirstOccurrence(String sentence, String search) {
 
